dd7e0adcd4f5e5aae8d220fa829c917d420d085e
nischelf <nischelwitzer.fabian@a1.net>
2024-04-29 09:34:22 +0200

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# Installation
+
+```bash
+pnpm install
+```
+
+# Usage
+
+## Start development server
+
+```bash
+pnpm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl">Todos</h1>
+    </main>
+  );
+}
+
+export default App;
