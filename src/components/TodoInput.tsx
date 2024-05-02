54fb1b21dba86de233af53edeea0f894be8acecd
nischelf <nischelwitzer.fabian@a1.net>
2024-05-02 09:33:35 +0200

    change width

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -20,41 +20,46 @@ export default function TodoInput({
   cancelHandler: () => void;
 }) {
   return (
-    <div className="fixed left-1/4 top-1/2 flex flex-col bg-white w-1/2">
-      <input
-        type="text"
-        placeholder="Title"
-        value={title}
-        onChange={titleChange}
-      />
-      <input
-        type="text"
-        placeholder="Details"
-        value={details || ""}
-        onChange={detailsChange}
-      />
-      {addMode ? (
+    // <div className="fixed left-0 top-0 flex w-full min-h-screen justify-center items-center">
+    <div className="fixed left-0 top-1/3 flex flex-col w-full justify-center items-center">
+      <div className="w-3/4 bg-white flex flex-col gap-2 rounded-lg p-2 items-center">
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={titleChange}
+          className="rounded border-2 border-gray-400 w-full p-1"
+        />
+        <input
+          type="text"
+          placeholder="Details"
+          value={details || ""}
+          onChange={detailsChange}
+          className="rounded border-2 border-gray-400 w-full p-1"
+        />
+        {addMode ? (
+          <button
+            onClick={addTodoHandler}
+            className="bg-green-500 rounded text-white p-0.5 w-full"
+          >
+            Hinzufügen
+          </button>
+        ) : (
+          <button
+            onClick={saveEditHandler}
+            className="bg-green-500 rounded text-white p-0.5 w-full"
+          >
+            Speichern
+          </button>
+        )}
         <button
-          onClick={addTodoHandler}
-          className="bg-green-500 rounded text-white p-0.5"
+          onClick={cancelHandler}
+          className="bg-red-500 rounded text-white p-0.5 w-full"
         >
-          Hinzufügen
+          Cancel
         </button>
-      ) : (
-        <button
-          onClick={saveEditHandler}
-          className="bg-green-500 rounded text-white p-0.5"
-        >
-          Speichern
-        </button>
-      )}
-      <button
-        onClick={cancelHandler}
-        className="bg-red-500 rounded text-white p-0.5"
-      >
-        Cancel
-      </button>
-      <p className="text-red-500">{errorMsg}</p>
+        <p className="text-red-500">{errorMsg}</p>
+      </div>
     </div>
   );
 }
